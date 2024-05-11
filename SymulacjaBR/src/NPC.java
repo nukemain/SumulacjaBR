f7038348ac647bc5a9214c7e81496eaf8a1f376c
nukemain <sowa.piotr15@gmail.com>
2024-05-11 03:47:28 +0200

    Czystki w Logic.java, tłumaczenie komentarzy

---
+++ SymulacjaBR/src/NPC.java
@@ -0,0 +1,13 @@
+public class NPC {
+    private int posX;
+    private int posY;
+    private int HP;
+    private int stamina;
+    //todo: broń
+    NPC(int posX,int posY){
+        this.posX = posX;
+        this.posY = posY;
+    }
+    //todo:wszystko
+
+}
